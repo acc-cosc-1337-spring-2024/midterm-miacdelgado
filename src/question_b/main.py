ca960d9eac1f0917ecde77c9e5fbe6db5f276fa3
miacdelgado <153124560+miacdelgado@users.noreply.github.com>
2024-03-05 20:10:00 +0000

    Question B answer

--- src/question_b/main.py
+++ src/question_b/main.py
@@ -1,1 +1,12 @@
-#add import
+import question_b
+
+# Prompts user for an int representing the day of the week
+day = int(input("Please enter a number 1 - 7 representing the day of the week: "))
+
+# This will check to see if the number entered is valid
+while day < 1 or day > 7:
+    day = int(input("The number entered is not valid. Please enter a number between 1-7: "))
+
+# This will display what the day of the week is
+day_result = question_b.get_day_of_week(day)
+print(day_result)
--- tests/question_tests/question_tests.py
+++ tests/question_tests/question_tests.py
@@ -2,11 +2,18 @@
 import unittest
 
 #follow this example to add questions b, c, and d for testing including their functions
-from src.question_a.question_a import test_config
+from src.question_b.question_b import get_day_of_week
 
 class Test_Config(unittest.TestCase):
 
-    def test_question_a_config(self):
-        self.assertEqual(True, test_config())
-
+    def test_question_b_test_1(self):
+        self.assertEqual('Invalid Number', get_day_of_week(0))
+    def test_question_b_tes_2(self):
+        self.assertEqual('Monday', get_day_of_week(1))
+    def test_question_b_test_3(self):
+        self.assertEqual('Tuesday', get_day_of_week(2))
+    def test_question_b_test_4(self):
+        self.assertEqual('Wednesday', get_day_of_week(3))
+    def test_question_b_test_5(self):
+        self.assertEqual('Invalid Number', get_day_of_week(8))
 
