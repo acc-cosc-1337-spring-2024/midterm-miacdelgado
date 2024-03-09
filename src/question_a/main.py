63bdcafbb16a1c6dc9055e51003e6ab2a668b3ee
miacdelgado <153124560+miacdelgado@users.noreply.github.com>
2024-03-09 19:02:33 +0000

    Question A

--- src/question_a/main.py
+++ src/question_a/main.py
@@ -1,1 +1,30 @@
-#add import
+import question_a
+import random
+
+rand_num = random.randrange(1,5)
+print(rand_num)
+
+print("Welcome to the random number guessing game.\n"
+      "I've thought of a number between 1 and 5.\n"
+      "And you have to guess it.")
+
+guess = int(input("Please guess a number: "))
+
+while guess < 1 or guess > 5:
+    guess = int(input("The number entered is out of range. Please choose a number between 1 and 5: "))
+
+while guess != rand_num:
+    compare_guess = question_a.get_random_number(guess, rand_num)
+
+    if guess > rand_num:
+        print(compare_guess)
+    elif guess < rand_num:
+        print(compare_guess)
+    elif guess == rand_num:
+        print(compare_guess)
+        break
+    
+    guess = int(input("Please guess a number: "))
+
+
+
--- src/question_a/question_a.py
+++ src/question_a/question_a.py
@@ -1,3 +1,8 @@
-#write functions here, don't add input('') statements here!
-def get_random_number(num):
-    
+def get_random_number(guess, rand_num):
+    while guess != rand_num:
+        if guess < rand_num:
+            return "The number guessed is larger. Try again."
+        elif guess > rand_num:
+            return "The number guess is smaller. Try again."
+        elif guess == rand_num:
+            return "You guessed the correct number!"
